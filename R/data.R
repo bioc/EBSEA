6c8b20af0ab99c15b3fea52e02f821629c47a5ba
arfamehmood <arfa.mehmood@utu.fi>
2020-10-23 22:30:59 +0300

    Adding source url to data file

--- R/data.R
+++ R/data.R
@@ -6,7 +6,7 @@
 #' @name exonCounts
 #' @keywords datasets
 #' @format A data frame with 1000 rows and 7 variables
-#' @source Exoncounts from Pasilla package
+#' @source Exoncounts from Pasilla package \url{https://bioconductor.org/packages/release/data/experiment/html/pasilla.html}
 #' @references Huber W, Reyes A (2020). pasilla: Data package with 
 #' per-exon and per-gene read counts of RNA-seq samples of Pasilla
 #' knock-down by Brooks et al., Genome Research 2011 
